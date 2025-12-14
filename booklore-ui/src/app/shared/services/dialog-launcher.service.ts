06ff59de667da4112861ee7c87d3451d3970bc4c
Muppetteer <richardson.jem+github@gmail.com>
2025-12-14 18:30:08 +1100

    fix(ui): library directory picker dialog double heading

--- booklore-ui/src/app/shared/services/dialog-launcher.service.ts
+++ booklore-ui/src/app/shared/services/dialog-launcher.service.ts
@@ -64,7 +64,7 @@ export class DialogLauncherService {
 
   openDirectoryPickerDialog(): DynamicDialogRef | null {
     return this.openDialog(DirectoryPickerComponent, {
-      header: 'Select Media Directory',
+      showHeader: false,
       styleClass: 'dynamic-dialog-minimal',
     });
   }
