bbbaed7289f3237279d38a08b4f7e6d95f6b4c74
Bryan Jones <unix.chkpwd@gmail.com>
2025-12-28 11:24:56 -0500

    chore: allow user to bind to any port host side

--- start.sh
+++ start.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env sh
 
-# Set default and export so envsubst sees it
-: "${BOOKLORE_PORT:=6060}"
-export BOOKLORE_PORT
-
-# Use envsubst safely
-TMP_CONF="/tmp/nginx.conf.tmp"
-envsubst '${BOOKLORE_PORT}' < /etc/nginx/nginx.conf > "$TMP_CONF"
-
-# Move to final location
-mv "$TMP_CONF" /etc/nginx/nginx.conf
-
 # Start nginx in background
 nginx -g 'daemon off;' &
 
