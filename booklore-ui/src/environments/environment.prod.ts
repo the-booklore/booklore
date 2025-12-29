e56ba3ea38f8e17ed65d0668449552ad3f0b520e
Jonas Bergler <jonas@bergler.name>
2025-12-29 18:12:29 +1300

    review feedback

--- booklore-ui/src/environments/environment.prod.ts
+++ booklore-ui/src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
+const base_path = document.querySelector('base')?.getAttribute('href')?.replace(/\/$/, '') || '';
 export const environment = {
   production: true,
   API_CONFIG: {
-    BASE_URL: window.location.origin + (document.querySelector('base')?.getAttribute('href')?.replace(/\/$/, '') || ''),
+    BASE_URL: window.location.origin + base_path,
     BROKER_URL:
       window.location.protocol === 'https:'
-        ? `wss://${window.location.host}${(document.querySelector('base')?.getAttribute('href')?.replace(/\/$/, '') || '')}/ws`
-        : `ws://${window.location.host}${(document.querySelector('base')?.getAttribute('href')?.replace(/\/$/, '') || '')}/ws`,
+        ? `wss://${window.location.host}${base_path}/ws`
+        : `ws://${window.location.host}${base_path}/ws`,
   },
 };
