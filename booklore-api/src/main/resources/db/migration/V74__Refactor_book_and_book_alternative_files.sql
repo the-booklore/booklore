bc25baa2cfa56e6e584cc5e001b9bab0daf07ebf
Sergio Visinoni <piffio@piffio.org>
2026-01-06 15:44:02 +0100

    fix(migration): remove the unique index before importing data

--- booklore-api/src/main/resources/db/migration/V74__Refactor_book_and_book_alternative_files.sql
+++ booklore-api/src/main/resources/db/migration/V74__Refactor_book_and_book_alternative_files.sql
@@ -7,6 +7,9 @@ CREATE TABLE book_file LIKE book_file_old;
 ALTER TABLE book_file ADD COLUMN is_book boolean DEFAULT false;
 ALTER TABLE book_file ADD column book_type varchar(32);
 
+-- Drop the column before importing the data to avoid duplicate index errors
+ALTER TABLE book_file DROP COLUMN alt_format_current_hash;
+
 INSERT INTO book_file (book_id, file_name, file_sub_path, is_book, book_type, file_size_kb, initial_hash, added_on, current_hash)
 SELECT id, file_name, file_sub_path, true, CASE when book_type = 0 then 'PDF' when book_type = 1 then 'EPUB' when book_type = 2 then 'CBX' end, file_size_kb, initial_hash, added_on, current_hash FROM book;
 
@@ -14,8 +17,6 @@ INSERT INTO book_file (book_id, file_name, file_sub_path, file_size_kb, initial_
 SELECT book_id, file_name, file_sub_path, file_size_kb, initial_hash, current_hash, description, added_on, additional_file_type FROM book_file_old;
 
 UPDATE book_file SET is_book = true WHERE additional_file_type = 'ALTERNATIVE_FORMAT';
-
-ALTER TABLE book_file DROP COLUMN alt_format_current_hash;
 ALTER TABLE book_file DROP COLUMN additional_file_type;
 
 -- Set book_type for existing book files
@@ -84,8 +85,9 @@ BEGIN
 END$$
 DELIMITER ;
 
--- Regenerate virtual column for alternative book format files
+-- Regenerate virtual column for alternative book format files, create the index without UNIQUE constraint
 ALTER TABLE book_file ADD COLUMN alt_format_current_hash VARCHAR(128) AS (CASE WHEN is_book = true THEN current_hash END) STORED;
+ALTER TABLE book_file ADD INDEX idx_book_file_current_hash_alt_format (alt_format_current_hash);
 
 -- Remove constraint from book table
 ALTER TABLE book DROP INDEX IF EXISTS unique_library_file_path;
@@ -98,4 +100,4 @@ ALTER TABLE book DROP COLUMN file_size_kb;
 ALTER TABLE book DROP COLUMN initial_hash;
 ALTER TABLE book DROP COLUMN current_hash;
 
- DROP TABLE book_file_old;
+-- DROP TABLE book_file_old;
