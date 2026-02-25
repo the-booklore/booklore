cadf8029c58361fd1ca104d8cfe5d0b84c8f235c
acx10 <acx10@users.noreply.github.com>
2026-02-25 13:31:05 -0700

    Fix Hibernate NPE in bookdrop notification by replacing entity findAll with scalar JPQL query

--- booklore-api/src/main/java/org/booklore/model/enums/PermissionType.java
+++ booklore-api/src/main/java/org/booklore/model/enums/PermissionType.java
@@ -1,23 +1,33 @@
 package org.booklore.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum PermissionType {
-    ADMIN,
-    UPLOAD,
-    DOWNLOAD,
-    EDIT_METADATA,
-    MANAGE_LIBRARY,
-    EMAIL_BOOK,
-    DELETE_BOOK,
-    SYNC_KOREADER,
-    SYNC_KOBO,
-    ACCESS_OPDS,
-    MANAGE_METADATA_CONFIG,
-    ACCESS_BOOKDROP,
-    ACCESS_LIBRARY_STATS,
-    ACCESS_USER_STATS,
-    ACCESS_TASK_MANAGER,
-    MANAGE_GLOBAL_PREFERENCES,
-    MANAGE_ICONS,
-    MANAGE_FONTS,
-    DEMO_USER
+
+    ADMIN("permissionAdmin"),
+    UPLOAD("permissionUpload"),
+    DOWNLOAD("permissionDownload"),
+    EDIT_METADATA("permissionEditMetadata"),
+    MANAGE_LIBRARY("permissionManageLibrary"),
+    EMAIL_BOOK("permissionEmailBook"),
+    DELETE_BOOK("permissionDeleteBook"),
+    SYNC_KOREADER("permissionSyncKoreader"),
+    SYNC_KOBO("permissionSyncKobo"),
+    ACCESS_OPDS("permissionAccessOpds"),
+    MANAGE_METADATA_CONFIG("permissionManageMetadataConfig"),
+    ACCESS_BOOKDROP("permissionAccessBookdrop"),
+    ACCESS_LIBRARY_STATS("permissionAccessLibraryStats"),
+    ACCESS_USER_STATS("permissionAccessUserStats"),
+    ACCESS_TASK_MANAGER("permissionAccessTaskManager"),
+    MANAGE_GLOBAL_PREFERENCES("permissionManageGlobalPreferences"),
+    MANAGE_ICONS("permissionManageIcons"),
+    MANAGE_FONTS("permissionManageFonts"),
+    DEMO_USER("permissionDemoUser");
+
+    private final String entityField;
+
+    PermissionType(String entityField) {
+        this.entityField = entityField;
+    }
 }
